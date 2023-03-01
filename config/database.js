0321f14b2890c3c9fdaaa39aefc919409aba66cb
Nam Ninja <namninja@gmail.com>
2023-03-01 14:47:36 -0800

    update resource

--- config/database.js
+++ config/database.js
@@ -2,9 +2,9 @@
 
 exports.DATABASE_URL =
   process.env.DATABASE_URL ||
-  "mongodb+srv://zero-admin:WutangClan@cluster0-pfvwv.mongodb.net/tealium-presentation?retryWrites=true";
+  "mongodb+srv://zero-admin:WutangClan@cluster0.pfvwv.mongodb.net/tealium-presentation?retryWrites=true";
 exports.TEST_DATABASE_URL =
   process.env.TEST_DATABASE_URL ||
-  "mongodb+srv://zero-admin:WutangClan@cluster0-pfvwv.mongodb.net/test-tealium-presentation?retryWrites=true";
+  "mongodb+srv://zero-admin:WutangClan@cluster0.pfvwv.mongodb.net/test-tealium-presentation?retryWrites=true";
 
 exports.PORT = process.env.PORT || 8080;
